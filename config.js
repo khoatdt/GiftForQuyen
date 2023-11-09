47e2273f5d5c6a7933faee2c6718de38fa80613d
Tuan Khoa <khoatdtce160799@fpt.edu.vn>
2023-11-09 22:22:19 +0700

    Upload source code

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+  introTitle: "Thảo Quyên à!",
+  introDesc: `Hôm nay chắc là Quyên đi học và có một ngày mệt mỏi lắm hã?
+    Khoa có một ít điều ngọt ngào muốn dành tặng Quyên nè.`,
+  btnIntro: "Bấm vào đây nè Quyên.",
+  title: "Thật ra Khoa chỉ muốn nói với Quyên đôi lời 😙",
+  desc: "Đây là lần đầu tiên, Khoa làm trang web này dành tặng cho một người đặc biệt. Khoa mong rằng khi xem trang web này Quyên sẽ thấy vui hơn và thích nó. Khoa nghĩ chắc Quyên sẽ biết những gì Khoa làm là vì điều gì mà?",
+  btnYes: "Quyên biết 😊",
+  btnNo: "Quyên không biết 😥",
+  question:
+    "Thật ra Khoa muốn hẹn Quyên đi uống nước á. Nếu như một ngày không xa Quyên có rảnh thì Quyên nói cho Khoa biết nha. Khoa sẽ đợi một ngày Quyên rảnh.",
+  btnReply: "Gửi cho Khoa nè",
+  reply: "Nếu như thứ 7 tuần này Quyên rảnh thì Khoa có thể hẹn Quyên đi uống nước được không? ",
+  mess: "Tất cả những gì Khoa làm cũng chỉ muốn Quyên được cười nhiều và vui hơn thôi.",
+  messDesc:
+    "Quyên à, Quyên có biết Quyên cười dễ thương lắm không. Cho nên là Quyên hãy cười thật nhiều lên nha (cười tươi như vậy nè 😁😁😁). Mấy ngày nay, trời cũng đang bắt đầu vào đông á Quyên, cho nên là Quyên đi ra ngoài hay đi học bài về trễ thì nhớ đem theo áo khoác kẻo bị bệnh nha.",
+  btnAccept: "Okiiiii Khoa <3",
+  messLink: "https://www.youtube.com/watch?v=ZqDBgYPpUTg",
+};
